daf149e93b08735a3744a6b9bbde76d5f4ace1be
Mohamed Jehad <mohamedjehaad@gmail.com>
2025-11-23 21:43:42 +0200

    UpdateProfile

--- Freeqy-APIs/Services/UserService.cs
+++ Freeqy-APIs/Services/UserService.cs
@@ -13,4 +13,57 @@ public class UserService(UserManager<ApplicationUser> userManager) : IUserServic
 
 		return Result.Success(user);
 	}
+
+	public async Task<Result<UserProfileResponse>> UpdateProfileAsync(string userId, UpdateUserProfileRequest request)
+	{
+		var user = await _userManager.FindByIdAsync(userId);
+
+		if (user is null)
+			return Result.Failure<UserProfileResponse>(UserErrors.UserNotFound);
+
+		// Check if data has actually changed
+		var hasChanges = user.FirstName != request.FirstName || user.LastName != request.LastName;
+
+		if (!hasChanges)
+		{
+			// Return current profile without updating
+			var currentProfile = new UserProfileResponse(
+				user.Email!,
+				user.UserName!,
+				user.FirstName,
+				user.LastName
+			);
+
+			return Result.Success(currentProfile);
+		}
+
+		// Update user properties
+		user.FirstName = request.FirstName;
+		user.LastName = request.LastName;
+
+		var result = await _userManager.UpdateAsync(user);
+
+		if (!result.Succeeded)
+		{
+			var error = result.Errors.FirstOrDefault();
+			
+			if (error is null)
+			{
+				return Result.Failure<UserProfileResponse>(
+					new Error("User.UpdateFailed", "Failed to update user profile", StatusCodes.Status500InternalServerError));
+			}
+
+			return Result.Failure<UserProfileResponse>(
+				new Error(error.Code, error.Description, StatusCodes.Status400BadRequest));
+		}
+
+		var response = new UserProfileResponse(
+			user.Email!,
+			user.UserName!,
+			user.FirstName,
+			user.LastName
+		);
+
+		return Result.Success(response);
+	}
 }
